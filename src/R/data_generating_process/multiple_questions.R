99fbd29184f6ef481741e1e1084973574a38d3d1
MerlinHeidemanns <45107044+MerlinHeidemanns@users.noreply.github.com>
2021-09-13 17:18:53 -0400

    commit before new branch

--- src/R/data_generating_process/multiple_questions.R
+++ src/R/data_generating_process/multiple_questions.R
@@ -26,5 +26,28 @@ sigma <- 0.01
 covmat <- matrix(rho * sigma^2, nrow = 4, ncol = 4)
 diag(covmat) <- sigma^2
 
-MASS::mvrnorm(1, p, covmat) * 1000
+apply(MASS::mvrnorm(1000, p, covmat) * 1000, 2, mean)
+
+#' Subsetting
+p <- c(0.3, 0.3, 0.1, 0.1, 0.2)
+covmat <- p %*% t(p)
+for (j in 1:length(p)){
+  for (i in 1:length(p)){
+    if (i != j){
+      covmat[i, j] = -covmat[i, j]
+    }
+  }
+}
+p = p * 200
+covmat = covmat * 200
+
+p[1:4] + (covmat[1:4, 5] %*% solve(covmat[5, 5])) %*% (0 - p[5])
+
+
+
+
+
+
+
+
 
