e1324b1ef610ac8079e791e7dcc288a0630568de
MerlinHeidemanns <45107044+MerlinHeidemanns@users.noreply.github.com>
2021-09-16 10:50:14 -0400

    Check distribution of polls by candidate

--- src/R/organizing/observed_polls/poll_checks.R
+++ src/R/organizing/observed_polls/poll_checks.R
@@ -42,6 +42,30 @@ if (previous_election_sum != 100){
   stop("Previous election result sum different from 100 percent.")
 }
 
+## Distribution by candidate
+dist_by_candidate <- df %>%
+  filter(round == 1) %>%
+  mutate(percentage = as.numeric(percentage)) %>%
+  filter(!is.na(percentage)) %>%
+  group_by(candidate) %>%
+  summarize(
+    q10 = quantile(percentage, 0.1),
+    q25 = quantile(percentage, 0.25),
+    q50 = quantile(percentage, 0.50),
+    q75 = quantile(percentage, 0.75),
+    q90 = quantile(percentage, 0.90),
+    min = min(percentage),
+    max = max(percentage)
+  )
+for (j in 1:nrow(dist_by_candidate)){
+  print(dist_by_candidate[j,] %>% as.data.frame())
+}
+
+
+
+
+
+
 
 
 
