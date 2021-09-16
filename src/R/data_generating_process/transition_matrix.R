a8a8e9ce537658d2ea3bb8f262ac1c6b81b5ba9d
MerlinHeidemanns <45107044+MerlinHeidemanns@users.noreply.github.com>
2021-09-16 10:51:14 -0400

    Some testing with the transition matrix

--- src/R/data_generating_process/transition_matrix.R
+++ src/R/data_generating_process/transition_matrix.R
@@ -18,7 +18,6 @@ mat[1:4, 1:4] - mat[1:4, 5] %*% solve(mat[5,5]) %*% mat[5, 1:4]
 mat <- abs(rnorm(5, 0, 1))
 #mat <- mat/sum(mat)
 mat <- mat %*% t(mat)
-mat <-
 #mat <- t(p) %*% p
 diag(mat) <- 0
 for (j in 1:nrow(mat)){
@@ -26,6 +25,26 @@ for (j in 1:nrow(mat)){
 }
 mat = mat * - 1
 diag(mat) <- 1
-
+sigma <- seq(1, 5)
+sigma <- sigma %*% t(sigma)
 pcond = p[1:3] + mat[1:3, 4:5] %*% solve(mat[4:5,4:5]) %*% (0 - p[4:5])
 sum(pcond)
+
+P <- 5
+corr_mat_positions <- matrix(0, nrow = P, ncol = P - 1)
+for (ii in 1:P){
+  for (jj in 1:P - 1){
+    if (jj >= ii){
+      corr_mat_positions[ii, jj] = jj + 1
+    } else {
+      corr_mat_positions[ii, jj] = jj
+    }
+  }
+}
+corr_mat_positions
+
+p[1:4] + MASS::mvrnorm(1, rep(0, 4), (mat * sigma)[1:4, 1:4])
+
+
+
+
