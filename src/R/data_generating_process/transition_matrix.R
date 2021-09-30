2a1d2ba4e2d407d739541536270c9c590499b969
MerlinHeidemanns <45107044+MerlinHeidemanns@users.noreply.github.com>
2021-09-30 12:35:42 -0400

    playing around with this a bit

--- src/R/data_generating_process/transition_matrix.R
+++ src/R/data_generating_process/transition_matrix.R
@@ -9,12 +9,12 @@ mat[3, 5] = mat[5, 3] = mat[3, 4] = mat[4, 3] = -0.1
 mat[4, 5] = mat[5, 4] = -0.1
 #mat[2, 3] = mat[3, 2] = mat[3, 1] = mat[1, 3] = -0
 mat
-pcond <- p[1:3] + mat[1:3, 4:5] %*% solve(mat[4:5,4:5]) %*% (0 - p[4:5])
+pcond <- p[1:3] + mat[1:3, 4:5] %*% solve(mat[4:5,4:5]) %*% (c(0.2, 0.2) - p[4:5])
 sum(pcond)
 mat
 mat[1:4, 1:4] - mat[1:4, 5] %*% solve(mat[5,5]) %*% mat[5, 1:4]
 
-
+p <- DirichletReg::rdirichlet(1, rep(1, 5))
 mat <- abs(rnorm(5, 0, 1))
 #mat <- mat/sum(mat)
 mat <- mat %*% t(mat)
@@ -27,8 +27,13 @@ mat = mat * - 1
 diag(mat) <- 1
 sigma <- seq(1, 5)
 sigma <- sigma %*% t(sigma)
-pcond = p[1:3] + mat[1:3, 4:5] %*% solve(mat[4:5,4:5]) %*% (0 - p[4:5])
+pcond = p[1:3] + mat[1:3, 4:5] %*% solve(mat[4:5,4:5]) %*% (c(0.3, 0.3) - p[4:5])
 sum(pcond)
+pcond
+p
+
+
+
 
 P <- 5
 corr_mat_positions <- matrix(0, nrow = P, ncol = P - 1)
