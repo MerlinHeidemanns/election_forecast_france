e33b15577d2f23d42eadd4f43b6df46d53d718d9
MerlinHeidemanns <45107044+MerlinHeidemanns@users.noreply.github.com>
2021-09-30 12:36:44 -0400

    adding popularity and the other thing to fundamentals simulation

--- src/R/functions/sim_fundamentals.R
+++ src/R/functions/sim_fundamentals.R
@@ -35,13 +35,24 @@ sim_fundamentals <- function(NElections, NBlocs){
   logodds_mu <- matrix(NA, nrow = NElections, ncol = NBlocs)
   prob_mu <- matrix(NA, nrow = NElections, ncol = NBlocs)
   incumbency <- matrix(0, nrow = NElections, ncol = NBlocs)
+  presidential_popularity <- rep(NA, NElections)
+
 
   ## Coefficients
   beta_incumbency <- -abs(rnorm(1, 0, 0.3))
+  beta_popularity <- abs(rnorm(1, 0.1, 0.01))
+  beta_unemployment <- -abs(rnorm(1, 0.1, 0.01))
+
+  beta_fundamentals <- matrix(c(beta_popularity,
+                                beta_unemployment), ncol = 1)
 
   ## Generate election outcomes
   #' population size
   populace <- runif(1, 1000000, 2000000)
+  popularity <- runif(NElections, 0.3, 0.7)
+  unemployment <- runif(NElections, 0.01, 0.1)
+  x_fundamentals <- matrix(cbind(popularity, unemployment), nrow = NElections)
+
 
   y <- matrix(NA, nrow = NElections, ncol = NBlocs)
 
@@ -52,7 +63,9 @@ sim_fundamentals <- function(NElections, NBlocs){
   y[1, ] <- rmultinom(1, populace, prob_mu[1, ])
   for (tt in 2:NElections){
     incumbency[tt, ] <- c(0, as.integer(y[tt - 1, 2:NBlocs] == max(y[tt - 1, 2:NBlocs])))
-    logodds_mu[tt, ] <- logodds_alpha[tt, ] + beta_incumbency * incumbency[tt, ]
+    logodds_mu[tt, ] <- logodds_alpha[tt, ] +
+      beta_incumbency * incumbency[tt, ] +
+      c(x_fundamentals[tt,] %*% beta_fundamentals) * incumbency[tt, ]
     prob_mu[tt, ] <- exp_softmax(logodds_mu[tt, ])
     y[tt, ] <- rmultinom(1, populace, prob_mu[tt, ])
   }
@@ -82,8 +95,11 @@ sim_fundamentals <- function(NElections, NBlocs){
     y_df = y_df,
     y_plt = y_plt,
     incumbency = incumbency,
+    x_fundamentals = x_fundamentals,
     sigma_alpha = data.frame(sigma_alpha = sigma_alpha,
                              bloc_id = 2:NBlocs),
-    beta_incumbency = beta_incumbency
+    beta_incumbency = beta_incumbency,
+    beta_fundamentals = data.frame(name = c("popularity", "unemployment"),
+                                  value = beta_fundamentals)
   )
 }
--- src/R/testing/v1_only_current_election.R/model_fit_fundamentals.R
+++ src/R/testing/v1_only_current_election.R/model_fit_fundamentals.R
@@ -6,7 +6,7 @@ library(cmdstanr)
 ## functions
 source("src/R/functions/sim_fundamentals.R")
 ## Generate data
-NElections <- 10
+NElections <- 20
 NBlocs <- 6
 list_df <- sim_fundamentals(NElections = NElections,
                             NBlocs = NBlocs)
@@ -18,7 +18,9 @@ data_list <- list(
   NElections_past_fundamentals = list_df$NElections,
   NBlocs = list_df$NBlocs,
   y_fundamentals = list_df$y,
-  incumbency = list_df$incumbency
+  incumbency = list_df$incumbency,
+  K = dim(list_df$x_fundamentals)[2],
+  x_fundamentals = list_df$x_fundamentals
 )
 ## Compile model
 mod <- cmdstan_model("src/stan/v1_fundamentals.stan")
@@ -44,8 +46,22 @@ ppc_fundamentals_epsilon(fit)
 ## beta
 source("src/R/functions/ppc_fundamentals_beta_incumbency.R")
 ppc_fundamentals_beta_incumbency(fit, true_beta = list_df$beta_incumbency)
-
-
+## beta fundamentals
+true_beta <- list_df$beta_fundamentals
+beta_fundamentals <- fit$summary("beta_fundamentals", ~ quantile(., c(0.1, 0.25, 0.5, 0.75, 0.9)))
+colnames(beta_fundamentals) <- c("variable", "q10", "q25", "q50", "q75", "q90")
+beta_fundamentals <- beta_fundamentals %>%
+  mutate(
+    name = true_beta$name[as.integer(str_match(variable, "([\\d]+)")[,2])]
+  ) %>%
+  dplyr::select(-variable) %>%
+  left_join(true_beta)
+ggplot(data = beta_fundamentals, aes(x = name)) +
+  geom_point(aes(y = q50)) +
+  geom_point(aes(y = value), color = "red", shape = 2) +
+  geom_errorbar(aes(ymin = q25, ymax = q75), size = 0.5, width = 0) +
+  geom_errorbar(aes(ymin = q10, ymax = q90), size = 0.25, width = 0) +
+  theme_light()
 
 
 
