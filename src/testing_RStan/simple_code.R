d882c2dfdd17183d2d682d88f43bcf46c4b811ad
MerlinHeidemanns <45107044+MerlinHeidemanns@users.noreply.github.com>
2021-11-01 13:14:46 -0400

    Deleting old stuff

--- src/testing_RStan/simple_code.R
+++
@@ -1,8 +0,0 @@
-
-NPolls <- 30
-T <- 100
-NElections <- 3
-
-tElections <- c(1, 1:100[1:100%%50 == 0]))
-tPolls <- sample(1:100[!1:100 %in% tElections], NPolls, replace = TRUE)
-nPolls <- rep(1000, NPolls)
